9347c13004336bd4d34abeef97ae622aa7053a5f
Vikas Kad <vikas.kad@wwindia.com>
2017-10-27 21:56:47 +0530

    displayed fav on fav list

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Quote} from "../../data/quote.interface";
+import {QuotesService} from "../services/quotes";
 
 @IonicPage()
 @Component({
@@ -7,6 +9,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
-  
+  quotes:Quote;
+  constructor(
+    private quoteService:QuotesService){
+
+  }
+  ionViewWillEnter(){
+    this.quotes=this.quoteService.getFavQuotes()
+  }
 
 }
